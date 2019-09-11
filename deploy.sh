e0bb1442e353e904f77fc2793a6be46f8a375b46
Jack-Billings <jack.billings@ibm.com>
2019-09-11 16:19:19 -0500

    pls work

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,6 @@ docker push jfbill/multi-client:$SHA
 docker push jfbill/multi-server:$SHA
 docker push jfbill/multi-worker:$SHA
 
-curl -sL https://ibm.biz/idt-installer | bash 
-ibmcloud login --apikey vVhU4H2-OHWPAZF26lm4fqcvzYKBlm9zGgVl_uyWbnqd -a cloud.ibm.com -r us-south -g Default
-ibmcloud plugin update kubernetes-service
-ibmcloud ks cluster-config --cluster blrb3vkd01blsvng8atg
-export KUBECONFIG=/Users/Jack.Billings@ibm.com/.bluemix/plugins/container-service/clusters/blrb3vkd01blsvng8atg/kube-config-dal10-complex-cluster.yml
-
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=jfbill/multi-server:$SHA
 kubectl set image deployments/client-deployment client=jfbill/multi-client:$SHA
