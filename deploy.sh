f3a55d7d23fde159cf7f4d352417c309fdae664f
Jack-Billings <jack.billings@ibm.com>
2019-09-12 12:39:29 -0500

    edited script file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,10 @@
-# docker build -t jfbill/multi-client:latest -t jfbill/multi-client:$SHA -f ./client/Dockerfile ./client
-# docker build -t jfbill/multi-server:latest -t jfbill/multi-server:$SHA -f ./server/Dockerfile ./server
-# docker build -t jfbill/multi-worker:latest -t jfbill/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t us.icr.io/jb-namespace/fib-repo:latest -t us.icr.io/jb-namespace/fib-repo:$SHA -f ./client/Dockerfile ./client
 
-# docker push jfbill/multi-client:latest
-# docker push jfbill/multi-server:latest
-# docker push jfbill/multi-worker:latest
+docker push us.icr.io/jb-namespace/fib-repo:latest
 
-# docker push jfbill/multi-client:$SHA
-# docker push jfbill/multi-server:$SHA
-# docker push jfbill/multi-worker:$SHA
+docker push us.icr.io/jb-namespace/fib-repo:$SHA
 
 kubectl apply -f k8s
-# kubectl set image deployments/server-deployment server=jfbill/multi-server:$SHA
-# kubectl set image deployments/client-deployment client=jfbill/multi-client:$SHA
-# kubectl set image deployments/worker-deployment worker=jfbill/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=us.icr.io/jb-namespace/fib-repo:$SHA
+kubectl set image deployments/client-deployment client=us.icr.io/jb-namespace/fib-repo:$SHA
+kubectl set image deployments/worker-deployment worker=us.icr.io/jb-namespace/fib-repo:$SHA
