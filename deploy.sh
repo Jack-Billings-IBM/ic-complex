fd1ead0107140c05e685f549754e2cf3aabeda34
Jack-Billings <jack.billings@ibm.com>
2019-09-12 12:46:05 -0500

    testing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-docker build -t us.icr.io/jb-namespace/fib-repo:latest -t us.icr.io/jb-namespace/fib-repo:$SHA -f ./client/Dockerfile ./client
+docker build -t us.icr.io/jb-namespace/fib-repo:multi-client:latest -t us.icr.io/jb-namespace/fib-repo:multi-client:$SHA -f ./client/Dockerfile ./client
 
-docker push us.icr.io/jb-namespace/fib-repo:latest
+# docker push us.icr.io/jb-namespace/fib-repo:latest
 
-docker push us.icr.io/jb-namespace/fib-repo:$SHA
+# docker push us.icr.io/jb-namespace/fib-repo:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=us.icr.io/jb-namespace/fib-repo:$SHA
-kubectl set image deployments/client-deployment client=us.icr.io/jb-namespace/fib-repo:$SHA
-kubectl set image deployments/worker-deployment worker=us.icr.io/jb-namespace/fib-repo:$SHA
+# kubectl set image deployments/server-deployment server=us.icr.io/jb-namespace/fib-repo:multi-server:$SHA
+# kubectl set image deployments/client-deployment client=us.icr.io/jb-namespace/fib-repo:multi-client:$SHA
+# kubectl set image deployments/worker-deployment worker=us.icr.io/jb-namespace/fib-repo:multi-worker:$SHA
